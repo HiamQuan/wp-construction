67121522406a5fcca06f83188b9d4d9851d12d3b
NguyenAnhQuan <ngongquan@gmail.com>
2025-11-03 18:29:01 +0700

    feat: home page

--- wp-content/themes/construction/functions.php
+++ wp-content/themes/construction/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * construction functions and definitions
  *
@@ -7,9 +8,9 @@
  * @package construction
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if (! defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
 /**
@@ -19,17 +20,18 @@ if ( ! defined( '_S_VERSION' ) ) {
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function construction_setup() {
+function construction_setup()
+{
 	/*
 		* Make theme available for translation.
 		* Translations can be filed in the /languages/ directory.
 		* If you're building a theme based on construction, use a find and replace
 		* to change 'construction' to the name of your theme in all the template files.
 		*/
-	load_theme_textdomain( 'construction', get_template_directory() . '/languages' );
+	load_theme_textdomain('construction', get_template_directory() . '/languages');
 
 	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+	add_theme_support('automatic-feed-links');
 
 	/*
 		* Let WordPress manage the document title.
@@ -37,21 +39,21 @@ function construction_setup() {
 		* hard-coded <title> tag in the document head, and expect WordPress to
 		* provide it for us.
 		*/
-	add_theme_support( 'title-tag' );
+	add_theme_support('title-tag');
 
 	/*
 		* Enable support for Post Thumbnails on posts and pages.
 		*
 		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		*/
-	add_theme_support( 'post-thumbnails' );
+	add_theme_support('post-thumbnails');
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'construction' ),
-			'menu-2' => esc_html__( 'Header', 'construction' ),
-			'menu-3' => esc_html__( 'Footer', 'construction' ),
+			'menu-1' => esc_html__('Primary', 'construction'),
+			'menu-2' => esc_html__('Header', 'construction'),
+			'menu-3' => esc_html__('Footer', 'construction'),
 		)
 	);
 
@@ -85,7 +87,7 @@ function construction_setup() {
 	);
 
 	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+	add_theme_support('customize-selective-refresh-widgets');
 
 	/**
 	 * Add support for core custom logo.
@@ -102,7 +104,7 @@ function construction_setup() {
 		)
 	);
 }
-add_action( 'after_setup_theme', 'construction_setup' );
+add_action('after_setup_theme', 'construction_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -111,22 +113,24 @@ add_action( 'after_setup_theme', 'construction_setup' );
  *
  * @global int $content_width
  */
-function construction_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'construction_content_width', 640 );
+function construction_content_width()
+{
+	$GLOBALS['content_width'] = apply_filters('construction_content_width', 640);
 }
-add_action( 'after_setup_theme', 'construction_content_width', 0 );
+add_action('after_setup_theme', 'construction_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function construction_widgets_init() {
+function construction_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'construction' ),
+			'name'          => esc_html__('Sidebar', 'construction'),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'construction' ),
+			'description'   => esc_html__('Add widgets here.', 'construction'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -134,22 +138,21 @@ function construction_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', 'construction_widgets_init' );
+add_action('widgets_init', 'construction_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function construction_scripts() {
-	wp_enqueue_style( 'construction-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'construction-style', 'rtl', 'replace' );
+function construction_scripts()
+{
+	wp_enqueue_style('construction-style', get_stylesheet_uri(), array(), _S_VERSION);
+	wp_style_add_data('construction-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'construction-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'construction_scripts' );
+add_action('wp_enqueue_scripts', 'construction_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -174,7 +177,6 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
